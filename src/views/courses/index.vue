d89286507d32cdb9eced976844167edce07b6063
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-01-03 23:24:25 +0800

    add binding for keyword field

--- src/views/courses/index.vue
+++ src/views/courses/index.vue
@@ -7,27 +7,43 @@
             <v-flex xs12 md3>
               <v-select
                 v-model="college"
-                :items="['學院', '文學院', '理學院', '社會科學學院', '工學院', '管理學院', '法學院', '教育學院', '其他']"
+                :items="[
+                  '學院',
+                  '文學院',
+                  '理學院',
+                  '社會科學學院',
+                  '工學院',
+                  '管理學院',
+                  '法學院',
+                  '教育學院',
+                  '其他'
+                ]"
                 label="學院"
               ></v-select>
             </v-flex>
 
             <v-flex xs12 md3>
-              <v-text-field
-                label="系所"
-
-              ></v-text-field>
+              <v-text-field label="系所"></v-text-field>
             </v-flex>
 
             <v-flex xs12 md4>
               <v-text-field
+                v-model="keyword"
+                v-validate="'max:16'"
+                data-vv-name="keyword"
+                data-vv-as="搜尋字詞"
+                :error-messages="$validator.errors.collect('keyword')"
                 label="課程名稱、代碼/教授姓名"
               >
               </v-text-field>
             </v-flex>
 
             <v-flex xs12 md2>
-              <v-btn :block="$vuetify.breakpoint.smAndDown" color="success">
+              <v-btn
+                :block="$vuetify.breakpoint.smAndDown"
+                color="success"
+                :disabled="isFormInvalid"
+              >
                 <v-icon>search</v-icon>
               </v-btn>
             </v-flex>
@@ -39,13 +55,16 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Mixins } from "vue-property-decorator";
+import FormMixin from "@/mixins/form.ts";
 
 @Component
-export default class Courses extends Vue {
+export default class Courses extends Mixins(FormMixin) {
   college: string = "";
 
   department: string = "";
+
+  keyword: string = "";
 
   submit() {
     return 1;
