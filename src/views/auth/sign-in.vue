ada8de1fedc793b53fbddb173be7f60786fb1fc8
BePsvPT <bepsvpt@protonmail.com>
2019-01-04 23:19:27 +0800

    add message for sign up section

--- src/views/auth/sign-in.vue
+++ src/views/auth/sign-in.vue
@@ -6,9 +6,10 @@
 
         <v-text-field
           v-model="username"
-          v-validate="'required|numeric|length:9'"
+          v-validate="'required|length:9,10'"
           data-vv-name="username"
           data-vv-as="帳號"
+          :disabled="isSignUp"
           :error-messages="$validator.errors.collect('username')"
           hint="單一入口/校友系統"
           label="帳號"
@@ -22,6 +23,7 @@
           @click:append="passwordVisible = !passwordVisible"
           data-vv-name="password"
           data-vv-as="密碼"
+          :disabled="isSignUp"
           :error-messages="$validator.errors.collect('password')"
           hint="單一入口/校友系統"
           label="密碼"
@@ -29,27 +31,37 @@
           :type="passwordVisible ? 'text' : 'password'"
         ></v-text-field>
 
-        <v-text-field
-          v-if="status === statusType.SignUp"
-          v-model="nickname"
-          v-validate="'required|length:3,12'"
-          data-vv-name="nickname"
-          data-vv-as="暱稱"
-          :error-messages="$validator.errors.collect('nickname')"
-          label="暱稱"
-          prepend-icon="far fa-id-card"
-        ></v-text-field>
+        <v-expand-transition>
+          <div v-if="isSignUp">
+            <p class="mt-2 subheading text-xs-center">
+              歡迎使用 CCU PLUS，就差一步囉！
+            </p>
 
-        <v-text-field
-          v-if="status === statusType.SignUp"
-          v-model="email"
-          v-validate="'required|email'"
-          data-vv-name="email"
-          data-vv-as="信箱"
-          :error-messages="$validator.errors.collect('email')"
-          label="信箱"
-          prepend-icon="fas fa-envelope"
-        ></v-text-field>
+            <v-text-field
+              v-model="nickname"
+              v-validate="'required|length:3,12'"
+              autofocus
+              data-vv-name="nickname"
+              data-vv-as="暱稱"
+              :error-messages="$validator.errors.collect('nickname')"
+              label="暱稱"
+              maxlength="12"
+              minlength="3"
+              prepend-icon="far fa-id-card"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="email"
+              v-validate="'required|email'"
+              data-vv-name="email"
+              data-vv-as="信箱"
+              :error-messages="$validator.errors.collect('email')"
+              label="信箱"
+              prepend-icon="fas fa-envelope"
+              type="email"
+            ></v-text-field>
+          </div>
+        </v-expand-transition>
 
         <div class="mt-3 text-xs-center">
           <v-btn
@@ -59,12 +71,12 @@
             :loading="false"
             type="submit"
           >
-            <template v-if="status === statusType.SignIn">
+            <template v-if="isSignIn">
               <v-icon small>fas fa-sign-in-alt</v-icon>
               <span class="ml-2">登入</span>
             </template>
 
-            <template v-else-if="status === statusType.SignUp">
+            <template v-else-if="isSignUp">
               <v-icon small>fas fa-user-plus</v-icon>
               <span class="ml-2">註冊</span>
             </template>
@@ -88,8 +100,6 @@ enum Status {
 
 @Component
 export default class SignIn extends Mixins(FormMixin) {
-  statusType = Status;
-
   status: Status = Status.SignIn;
 
   username: string = "";
@@ -102,6 +112,14 @@ export default class SignIn extends Mixins(FormMixin) {
 
   email: string = "";
 
+  get isSignIn() {
+    return this.status === Status.SignIn;
+  }
+
+  get isSignUp() {
+    return this.status === Status.SignUp;
+  }
+
   async submit(event: Event) {
     event.preventDefault();
 
@@ -110,6 +128,8 @@ export default class SignIn extends Mixins(FormMixin) {
     }
 
     this.signIn();
+
+    this.status = Status.SignUp;
   }
 
   signIn() {
