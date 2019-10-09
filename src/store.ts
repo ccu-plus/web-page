d287523a508c20c21d39e943ddf40011598942ab
BePsvPT <bepsvpt@protonmail.com>
2019-10-09 22:57:39 +0800

    update store

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,33 @@
-import Vue from "vue";
-import Vuex from "vuex";
-import { CancelTokenSource } from "axios/index";
+import axios from '@/libs/axios';
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { CancelTokenSource } from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    requests: <Array<CancelTokenSource>>[]
+    requests: [] as CancelTokenSource[],
+    courses: [],
+    search: {
+      college: '',
+      department: '',
+      dimension: '',
+      keyword: '',
+    },
   },
-  mutations: {},
-  actions: {}
+
+  mutations: {
+    setCourses(state, payload) {
+      state.courses = payload;
+    },
+  },
+
+  actions: {
+    async search({ commit }, params) {
+      const { data: { data } } = await axios.get('/courses/search', { params });
+
+      commit('setCourses', data);
+    },
+  },
 });
