be89c34b712faa02e739415be5a21ad1d7fbdc2a
BePsvPT <bepsvpt@protonmail.com>
2019-10-09 23:00:42 +0800

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,17 @@
-import "./style.scss";
+import './style.scss';
 
-import Vue from "vue";
-import VeeValidate from "vee-validate";
-import Vuetify from "vuetify";
-import App from "./app.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
-
-Vue.use(VeeValidate, {
-  dictionary: {
-    zh_TW: require("vee-validate/dist/locale/zh_TW")
-  },
-  inject: false,
-  locale: "zh_TW"
-});
-
-Vue.use(Vuetify, {
-  lang: {
-    locales: {
-      "zh-Hant": require("vuetify/es5/locale/zh-Hant")
-    },
-    current: "zh-Hant"
-  }
-});
+import Vue from 'vue';
+import App from './app.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  name: 'CCUPLUS',
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#ccu-plus');
