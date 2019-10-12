5d69d97834eae1acbbbc5a629d35f153b9771375
BePsvPT <bepsvpt@protonmail.com>
2019-10-12 16:48:56 +0800

    change validation to eager mode

--- src/libs/validate.ts
+++ src/libs/validate.ts
@@ -1,4 +1,4 @@
-import { extend, localize, ValidationObserver, ValidationProvider } from 'vee-validate';
+import { extend, localize, setInteractionMode, ValidationObserver, ValidationProvider } from 'vee-validate';
 import { digits, email, max, min, required } from 'vee-validate/dist/rules';
 import zhTW from 'vee-validate/dist/locale/zh_TW.json';
 
@@ -9,5 +9,7 @@ extend('min', min);
 extend('required', required);
 
 localize('zhTW', zhTW);
+
+setInteractionMode('eager');
 
 export { ValidationObserver, ValidationProvider };
