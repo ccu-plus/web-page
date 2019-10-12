1589467b1be97808141e1b75704f145a2e7a8d0f
BePsvPT <bepsvpt@protonmail.com>
2019-10-13 00:46:27 +0800

    add sign-out view

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import Home from '@/views/home.vue';
 import Courses from '@/views/courses/index.vue';
 import CourseShow from '@/views/courses/show.vue';
 import SignIn from '@/views/auth/sign-in.vue';
+import SignOut from '@/views/auth/sign-out.vue';
 import NotFound from '@/views/errors/404.vue';
 
 export default [
@@ -24,6 +25,11 @@ export default [
     path: '/sign-in',
     name: 'sign-in',
     component: SignIn,
+  },
+  {
+    path: '/sign-out',
+    name: 'sign-out',
+    component: SignOut,
   },
   {
     path: '*',
---
+++ src/views/auth/sign-out.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row align="center" class="h-full" justify="center">
+    <v-progress-circular
+      color="primary"
+      indeterminate
+      :size="64"
+      :width="2"
+    />
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Route } from 'vue-router';
+
+Component.registerHooks([
+  'beforeRouteEnter',
+]);
+
+@Component
+export default class SignOut extends Vue {
+  public from!: Route;
+
+  private beforeRouteEnter(to: Route, from: Route, next: (to?: ((vm: SignOut) => any)) => void) {
+    next((vm) => vm.from = from);
+  }
+
+  private created() {
+    // sign out
+
+    this.$nextTick(() => this.$router.replace(this.from.path));
+  }
+}
+</script>
