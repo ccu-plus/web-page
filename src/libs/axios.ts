7d7403ef9f5b09fdf2b9d6747c3956b0c69bba81
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-10-12 14:46:53 +0800

    update axios validate status

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -4,6 +4,7 @@ import store from '@/store';
 
 const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
+  validateStatus: (status) => status >= 200 && status < 500,
 });
 
 instance.interceptors.request.use(
