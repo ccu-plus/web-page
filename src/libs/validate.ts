16ed07be870428603a06e36d35f722763bf885da
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-10-13 14:56:58 +0800

    add oneOf validation rule

--- src/libs/validate.ts
+++ src/libs/validate.ts
@@ -1,11 +1,12 @@
 import { extend, localize, setInteractionMode, ValidationObserver, ValidationProvider } from 'vee-validate';
-import { digits, email, max, min, required } from 'vee-validate/dist/rules';
+import { digits, email, max, min, oneOf, required } from 'vee-validate/dist/rules';
 import zhTW from 'vee-validate/dist/locale/zh_TW.json';
 
 extend('digits', digits);
 extend('email', email);
 extend('max', max);
 extend('min', min);
+extend('oneOf', oneOf);
 extend('required', required);
 
 localize('zhTW', zhTW);
