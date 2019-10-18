4d3eedbb3aa65b7b682cc41464acebf8af672bd9
BePsvPT <bepsvpt@protonmail.com>
2019-10-18 23:35:07 +0800

    show latest n comments in course search page

--- src/views/courses/index.vue
+++ src/views/courses/index.vue
@@ -74,58 +74,92 @@
     </validation-observer>
 
     <v-data-table
-        class="elevation-1"
-        disable-pagination
-        disable-sort
-        :headers="headers"
-        hide-default-footer
-        :items="$store.state.courses"
-        :loading="loading"
-        no-data-text="探索，帶來無限可能"
-      >
-        <template v-slot:item.recently="{ item: { recently } }">
-          <v-icon v-if="recently" color="green">{{ icons.mdiNewBox }}</v-icon>
-        </template>
+      class="elevation-1"
+      disable-pagination
+      disable-sort
+      :headers="headers"
+      hide-default-footer
+      :items="$store.state.courses"
+      :loading="loading"
+      no-data-text="探索，帶來無限可能"
+    >
+      <template v-slot:item.recently="{ item: { recently } }">
+        <v-icon v-if="recently" color="green">{{ icons.mdiNewBox }}</v-icon>
+      </template>
 
-        <template v-slot:item.code="{ item: { code, dimension } }">
-          <span class="d-block">{{ code }}</span>
-          <span v-if="dimension">{{ dimension }}</span>
-        </template>
+      <template v-slot:item.code="{ item: { code, dimension } }">
+        <span class="d-block">{{ code }}</span>
+        <span v-if="dimension">{{ dimension }}</span>
+      </template>
 
-        <template v-slot:item.name="{ item: { code, name } }">
-          <router-link :to="{ name: 'courses.show', params: { code } }">{{ name }}</router-link>
-        </template>
+      <template v-slot:item.name="{ item: { code, name } }">
+        <router-link :to="{ name: 'courses.show', params: { code } }">{{ name }}</router-link>
+      </template>
 
-        <template v-slot:item.professors="{ item: { professors } }">
-          <span
-            v-for="professor in professors.slice(0, 3)"
-            class="d-block"
-          >{{ professor }}</span>
+      <template v-slot:item.professors="{ item: { professors } }">
+        <span
+          v-for="professor in professors.slice(0, 3)"
+          class="d-block"
+        >{{ professor }}</span>
 
-          <span v-if="professors.length > 3">……</span>
-        </template>
+        <span v-if="professors.length > 3">……</span>
+      </template>
 
-        <template v-if="searched" v-slot:footer>
-          <div class="v-data-footer px-4 py-2">
-            <a
-              class="pt-1"
-              href="https://www.algolia.com"
-              rel="noopener noreferrer"
-              target="_blank"
+      <template v-if="searched" v-slot:footer>
+        <div class="v-data-footer px-4 py-2">
+          <a
+            class="pt-1"
+            href="https://www.algolia.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              alt="Search by algolia"
+              :src="require('@/assets/search-by-algolia.svg')"
             >
-              <img
-                alt="Search by algolia"
-                :src="require('@/assets/search-by-algolia.svg')"
-              >
-            </a>
-          </div>
+          </a>
+        </div>
+      </template>
+    </v-data-table>
+
+    <v-card
+      v-for="comment in comments"
+      class="mt-6"
+      outlined
+      tag="section"
+    >
+      <v-card-title class="subtitle-1">
+        <router-link :to="{ name: 'courses.show', params: { code: comment.course.code } }">
+          <span>{{ comment.course.department }}</span>
+          <span class="mx-1">-</span>
+          <span>{{ comment.course.name }}</span>
+        </router-link>
+
+        <span class="mx-2">·</span>
+
+        <span class="cyan--text text--darken-2">{{ comment.user || '匿名' }}</span>
+
+        <div class="flex-grow-1" />
+
+        <template v-if="comment.professor">
+          <span class="grey--text text--darken-1">受評教授：{{ comment.professor }}</span>
+
+          <span class="mx-2 grey--text text--lighten-1">·</span>
         </template>
-      </v-data-table>
+
+        <span class="grey--text">{{ comment.commented_at }}</span>
+      </v-card-title>
+
+      <v-divider />
+
+      <v-card-text style="user-select: none; white-space: pre-line;">{{ comment.content.trim() }}</v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import axios from '@/libs/axios';
 import Colleges from '@/libs/colleges';
 import Departments from '@/libs/departments';
 import Dimensions from '@/libs/dimensions';
@@ -139,6 +173,8 @@ import { ValidationProvider, ValidationObserver } from '@/libs/validate';
   },
 })
 export default class Courses extends Vue {
+  private comments = [];
+
   private form = this.$store.state.search;
 
   private icons = {
@@ -183,6 +219,12 @@ export default class Courses extends Vue {
     this.loading = false;
 
     this.searched = true;
+  }
+
+  private async created() {
+    const { data: { data } } = await axios.get('/courses/comments');
+
+    this.comments = data;
   }
 }
 </script>
