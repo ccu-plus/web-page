788d5823d1c3ffe03a469c1f37a74022e3d139ee
BePsvPT <bepsvpt@protonmail.com>
2019-10-19 17:15:23 +0800

    add profile view

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,21 @@
-import Home from '@/views/home.vue';
 import Courses from '@/views/courses/index.vue';
 import CourseShow from '@/views/courses/show.vue';
+import Home from '@/views/home.vue';
+import NotFound from '@/views/errors/404.vue';
+import Profile from '@/views/profile/index.vue';
 import SignIn from '@/views/auth/sign-in/index.vue';
 import SignOut from '@/views/auth/sign-out.vue';
-import NotFound from '@/views/errors/404.vue';
 
 export default [
   {
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
   },
   {
     path: '/courses',
