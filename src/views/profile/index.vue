e6f37404132d6d012d7998f2f6f34de401170575
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-10-19 17:15:23 +0800

    add profile view

---
+++ src/views/profile/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>暱稱：{{ profile.nickname }}</p>
+    <p>信箱：{{ profile.email }}（{{ profile.verified ? '已驗證' : '尚未驗證' }}）</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Profile extends Vue {
+  get profile() {
+    return this.$store.state.profile;
+  }
+}
+</script>
