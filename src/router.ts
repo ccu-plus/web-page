fd19591668abb28dd6db1049e008b590e2028eb9
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-10-23 20:10:25 +0800

    remove axios cancel token

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,5 @@
 import Router from 'vue-router';
 import routes from './routes';
-import store from './store';
 import Vue from 'vue';
 
 Vue.use(Router);
@@ -13,10 +12,6 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-  store.state.requests.forEach((xhr) => xhr.cancel());
-
-  store.state.requests = [];
-
   if (to.meta.title) {
     document.title = `${to.meta.title} | CCU PLUS`;
   } else {
